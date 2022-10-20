09392029a6d6b03263be6a0682988813f5f7ec43
Hoornito <agus.fernandez.y@gmail.com>
2022-10-20 13:21:22 -0300

    Se agrega clase ApiErrors

--- Pagos_System/MinimalAPI_Pagos/Endpoints/Errors/ApiError.cs
+++ Pagos_System/MinimalAPI_Pagos/Endpoints/Errors/ApiError.cs
@@ -2,5 +2,15 @@
 {
     public class ApiError
     {
+        public ApiError(string message, string innerMessage, string stackTrace)
+        {
+            this.Message = message;
+            this.InnerMessage = innerMessage;
+            this.StackTrace = stackTrace;
+        }
+
+        public string Message { get; set; }
+        public string InnerMessage { get; set; }
+        public string StackTrace { get; set; }
     }
 }
